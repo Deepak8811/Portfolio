510f47b51a4ebfab8bb94695937e319e8e7caf48
Deepak <deepchaurasiya1997@gmail.com>
2022-06-25 07:31:35 +0530

    Second commit

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function () {
 
   // typing text animation script
   var typed = new Typed(".typing", {
-    strings: ["Web Developer", "Programmer", " Web Designer"],
+    strings: ["Web Developer", "Programmer", " Web Designer","UI/UX Designer"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
   });
 
   var typed = new Typed(".typing-2", {
-    strings: ["Web Developer", "Programmer", "Web Designer"],
+    strings: ["Web Developer", "Programmer", "Web Designer","UI/UX Designer"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
